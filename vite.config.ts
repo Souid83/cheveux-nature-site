0f8c34fd8bb9aa39662cfc7a4a2e0e638a2f787c
Souid83 <swuidy@hotmail.fr>
2025-03-16 17:25:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cheveux-nature-site/", // Ajout du chemin pour GitHub Pages
+  base: "/cheveux-nature-site/", // IMPORTANT : Ajout du bon chemin pour GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
